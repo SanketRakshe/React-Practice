a6df7345ee48b5ebb6ef8884095eebdd2b35decb
sanketrakshe <sanketrakshe07@gmail.com>
2024-07-04 14:40:24 +0530

    Added: html, css and javascript things

---
+++ App.js
@@ -0,0 +1,10 @@
+// Manipulate the HTML DOM using Javscript
+
+const heading = document.createElement("h1");
+
+heading.innerHTML = "Namaste Everyone";
+
+const root = document.getElementById("root");
+
+root.appendChild(heading);
+
